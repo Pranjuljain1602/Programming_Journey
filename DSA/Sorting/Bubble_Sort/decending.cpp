4cddf3aa91ebba6db657b58e84b881f7f78a75f6
Pranjul Jain <107295419+Pranjuljain1602@users.noreply.github.com>
2024-01-23 18:59:57 +0530

    DSA Sorting

---
+++ DSA/Sorting/Bubble_Sort/decending.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of an array: ";
+    cin>>n;
+
+    cout<<"Enter the element of an array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+
+
+    for(int i=n-2; i>=0; i--)
+    {   
+        bool swapped = 0;
+        for(int j=0; j<=i; j++)
+        {   
+            swapped = 1;
+            if(arr[j]<arr[j+1])
+            swap(arr[j],arr[j+1]);
+        }
+
+        if(swapped==0)
+        break; 
+    }
+
+
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ DSA/Sorting/Seletion_Sort/ascending_char.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char arr[6] = {'e', 'g', 'a', 'z', 'b', 'f'};
+
+    // Print the original array
+    cout << "Original array: ";
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // Selection Sort algorithm
+    for (int i = 0; i < 5; i++) {
+        int index = i;
+
+        for (int j = i + 1; j < 6; j++) {
+            // Find the index of the minimum element in the unsorted portion
+            if (arr[j] < arr[index]) {
+                index = j;
+            }
+        }
+
+        // Swap the minimum element with the first element of the unsorted portion
+        swap(arr[i], arr[index]);
+
+        // Print the array after each iteration
+        cout << "After iteration " << i + 1 << ": ";
+        for (int k = 0; k < 6; k++) {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+    }
+
+    // Print the sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
